73e59772d78fc053169fb6208155661913ce6727
Ole Martin Gjersvik <olgj@equinor.com>
2018-11-12 15:42:50 +0100

    Input have gamepad support

--- www/app.mjs
+++ www/app.mjs
@@ -1,47 +1,17 @@
-let plussButtons = [false,false,false,false,false,false];
-let minusButtons = [false,false,false,false,false,false];
-let uiPos = [0,0,0,0,0,0];
-let serverPos = [0,0,0,0,0,0];
-let robotPos = [0,0,0,0,0,0];
+import { Input } from "./input.mjs";
 
-// setup buttons
-function setupButtons() {
-    let buttons = document.querySelectorAll("#add_button_row button");
-    var index = 0;
-    buttons.forEach(button => {
-        var i = index;
-        button.addEventListener("mousedown", e => plussButtons[i] = true);
-        button.addEventListener("mouseup", e => plussButtons[i] = false);
-        index += 1;
-    });
+const input = new Input();
+input.setup();
 
-    buttons = document.querySelectorAll("#sub_button_row button");
-    index = 0;
-    buttons.forEach(button => {
-        var i = index;
-        button.addEventListener("mousedown", e => minusButtons[i] = true);
-        button.addEventListener("mouseup", e => minusButtons[i] = false);
-        index += 1;
-    });
-}
-setupButtons();
-
-function updateUiPos(){
-    let pluss = plussButtons.map(p => p ? 1 : 0);
-    let minus = minusButtons.map(m => m ? -1 : 0);
-    uiPos = pluss.map((v, i) => v + minus[i]);
-}
-
-function showUiPos(){
-    for (let i = 0; i < uiPos.length; i++) {
+function showUiPos(inputs){
+    for (let i = 0; i < inputs.length; i++) {
         let elem = document.getElementById("ui"+ (i + 1))
-        elem.textContent = uiPos[i].toString();
+        elem.textContent = inputs[i].toFixed(3);
     }
 }
 
 function step(timestamp){
-    updateUiPos();
-    showUiPos();
+    showUiPos(input.getInputs());
     window.requestAnimationFrame(step);
 }
 
