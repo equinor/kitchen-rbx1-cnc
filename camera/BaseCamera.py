23439afc4e7b092c1d0f1769a0ee59e65892e402
Olav Vatne <olavvatne@gmail.com>
2018-11-17 21:18:25 +0100

    Web cam feed as motion jpeg - opencv video capture loop - multipart video-feed as image endpoint - Add simple css layout framework

---
+++ camera/BaseCamera.py
@@ -0,0 +1,47 @@
+import time
+import threading
+import signal
+
+class BaseCamera(object):
+    thread = None  # background thread that reads frames from camera
+    frame = None  # current frame is stored here by background thread
+    lastAccess = 0  # time of last client access to the camera
+    killCamera = False
+
+    def __init__(self):
+        if BaseCamera.thread is None:
+            # start background frame thread
+            BaseCamera.thread = threading.Thread(target=self._thread)
+            signal.signal(signal.SIGINT, self.handler)
+            BaseCamera.thread.start()
+
+    def handler(self):
+        if BaseCamera.thread is not None:
+            BaseCamera.killCamera = True
+
+    @staticmethod
+    def frames():
+        """Generator that returns frames from the camera."""
+        raise NotImplementedError('Must be implemented by subclasses.')
+
+    def getFrame(self):
+        """Return the current camera frame."""
+        BaseCamera.lastAccess = time.time()
+        time.sleep(0.1)
+        return BaseCamera.frame
+
+    @classmethod
+    def _thread(cls):
+        """Camera background thread."""
+        print('Starting camera thread.')
+        framesIterator = cls.frames()
+        for frame in framesIterator:
+            BaseCamera.frame = frame
+
+            # if there hasn't been any clients asking for frames in
+            # the last 10 seconds then stop the thread
+            if time.time() - BaseCamera.lastAccess > 10:
+                framesIterator.close()
+                print('Stopping camera thread due to inactivity.')
+                break
+        BaseCamera.thread = None
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import Rbx1cnc
 import sys
+from camera import Camera
 
 server = Rbx1cnc.Server(Rbx1cnc.Robot())
 server.start()
